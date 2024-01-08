4c3118284a9086c452239be59df57ea1ef2afc5b
AraujoTT <luisthiago642@gmail.com>
2024-01-07 21:32:18 -0300

    add start with grade file and finishe the leason with compare.py

--- conditions/compare.py
+++ conditions/compare.py
@@ -1,2 +1,9 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
+
+if x < y:
+    print('X is less than y')
+elif x > y:
+    print('X is greater than y')
+else:
+    print('X is equal to y')
