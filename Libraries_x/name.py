4ad5cf2c83fef4193db0a7a4897cd9ba6f78e7e8
AraujoTT <luisthiago642@gmail.com>
2024-02-02 02:32:16 -0300

    using for with sysargv and leaning how to use sys.exit

--- Libraries_x/name.py
+++ Libraries_x/name.py
@@ -1,13 +1,20 @@
 import sys
 
 if len(sys.argv)< 2:
-    print("Too few arguments")
-elif  len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("hello, my name is", sys.argv[1])
+    sys.exit("Too few arguments")
+for arg in sys.argv[1:]:
+    print("hello, my name is",arg )
 
+#////---////---////---////
+# if len(sys.argv)< 2:
+#     sys.exit("Too few arguments")
+# elif  len(sys.argv) > 2:
+#     sys.exit("Too many arguments")
+
+# print("hello, my name is", sys.argv[1])
+# //////--////---////---////
 # try:
 #     print("hello, my name is", sys.argv[1])
 # except IndexError:
 #     print("Too few arguments")
+
