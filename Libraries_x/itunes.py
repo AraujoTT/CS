e46a0226c0cbeba77570724d66b0df5683d917ec
AraujoTT <luisthiago642@gmail.com>
2024-02-02 20:55:22 -0300

    inplemanting how to find things insede json files

--- Libraries_x/itunes.py
+++ Libraries_x/itunes.py
@@ -1,8 +1,13 @@
+import json
 import requests
 import sys
 
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(response.json())
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+
+o = response.json()
+
+for obj in o['results']:
+    print(obj['trackName'])
