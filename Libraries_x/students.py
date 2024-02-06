bb28c2b68994ee9734a94fef71f1758870f52cf0
AraujoTT <luisthiago642@gmail.com>
2024-02-05 23:29:43 -0300

    improving code formatting with 'black', and starting unit tests

---
+++ Libraries_x/students.py
@@ -0,0 +1,9 @@
+students = {
+    "hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+    "Padma": "Ravenclaw"
+}
+for student in students:
+    print(student)
