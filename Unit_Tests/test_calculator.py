191f7a95c417a9da618e2ddd0405235c1ea39336
AraujoTT <luisthiago642@gmail.com>
2024-02-08 23:48:55 -0300

    using pytest to creat new test

--- Unit_Tests/test_calculator.py
+++ Unit_Tests/test_calculator.py
@@ -1,30 +1,12 @@
 from calculator import square
 
-def main():
-    test_square()
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
 
-def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 square was not 4")
-    try:
-        assert square(3) == 9
-    except:
-        print("3 square was not 9")
-    try:
-        assert square(-2) == 4
-    except:
-        print("-2 square was not 4")
-    try:
-        assert square(-3) == 9
-    except:
-        print("-3 square was not 9")
-    try:
-        assert square(0) == 0
-    except:
-        print("0 square was not 0")
+def test_neagtive():
+    assert square(-2) == 4
+    assert square(-3) == 9
 
-if __name__ == "__main__":
-    main()
-    
+def test_zero():
+    assert square(0) == 0
