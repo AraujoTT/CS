1fe8678d6ed1bf0f7aede5d1ab2c21f93d81043c
AraujoTT <luisthiago642@gmail.com>
2024-02-13 23:58:38 -0300

    learnig how to read a file using open function and how to append too

--- I_AND_O/names.py
+++ I_AND_O/names.py
@@ -1,10 +1,18 @@
-name = input("What's your name? ")
+with open("names.txt", "r") as file:
+    lines = file.readlines()
 
-with open("names.txt", "a") as file:
-    file.write(f"{name}\n")
+for line in lines:
+    print("helli,", line, end="")
+
+
+# Programing to append a name to a file
+# name = input("What's your name? ")
+# with open("names.txt", "a") as file:
+#     file.write(f"{name}\n")
 
 
 
+# Programing to append a name to a list
 # names = []
 
 # for _ in range(3):
