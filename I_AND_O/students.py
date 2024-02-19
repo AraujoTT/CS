09c43cb124b2b372275f9e79d510e8eae3a1715f
AraujoTT <luisthiago642@gmail.com>
2024-02-18 23:24:35 -0300

    imlementingg how to sorted elements insed the dictionary  and learning more abouse creat it too

--- I_AND_O/students.py
+++ I_AND_O/students.py
@@ -1,4 +1,22 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+def get_house(student):
+    return student["house"]
+
+for student in sorted(students, key=get_house):
+    print(f"{student['name']} is in {student['house']}")
+
+
+
+# Open a file and read after show the name of the student
+# and the house
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         print(f"{name} is in {house}")
