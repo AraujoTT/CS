43223a9a0b067eb6c19ca0e46c0a6a6293a03b87
AraujoTT <luisthiago642@gmail.com>
2024-02-28 22:06:19 -0300

    creating a program where it write/append insede the csv file

--- I_AND_O/students.py
+++ I_AND_O/students.py
@@ -1,22 +1,33 @@
 import csv
 
-students = []
+name = input("What's your name? ")
+home = input("where's your home? ")
 
-with open("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row["name"], "home":row["home"]})
+with open("students.csv", "a", newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
 
-    # for line in file:
-    #     name, home = line.rstrip().split(",")
-    #     student = {"name": name, "home": home}
-    #     students.append(student)
 
-# def get_name(student):
-#     return student["name"]
+# code using for read a csv file with csv,DictReader introducing how to read a cv file with name of columns and save in a dict
+# import csv
 
-for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+# students = []
+
+# with open("students.csv") as file:
+#     reader = csv.DictReader(file)
+#     for row in reader:
+#         students.append({"name": row["name"], "home":row["home"]})
+
+#     # for line in file:
+#     #     name, home = line.rstrip().split(",")
+#     #     student = {"name": name, "home": home}
+#     #     students.append(student)
+
+# # def get_name(student):
+# #     return student["name"]
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#     print(f"{student['name']} is from {student['home']}")
 
 
 
