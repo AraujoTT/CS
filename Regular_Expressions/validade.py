3b59181dd3a7ec3378665d9663091bbb7f84ddd0
AraujoTT <luisthiago642@gmail.com>
2024-03-07 23:00:51 -0300

    starting writing a code using a re library

--- Regular_Expressions/validade.py
+++ Regular_Expressions/validade.py
@@ -1,11 +1,19 @@
+import re
+
 email = input("What's your email? ").strip()
 
-username, domain = email.split("@")
-
-if username and domain.endswith(".edu"):
-    print('Valid')
+if re.search(".+@.+", email):
+    print("Valid")
 else:
     print("Invalid")
+
+#creating a new method to check if the entered string is an email address
+# username, domain = email.split("@")
+
+# if username and domain.endswith(".edu"):
+#     print('Valid')
+# else:
+#     print("Invalid")
 
 # basic example how to search if the string is a email or not
 # if "@" in email and "." in email:
