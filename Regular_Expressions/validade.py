44658c0e129a7074debf8f3ade5fd87590021e86
AraujoTT <luisthiago642@gmail.com>
2024-03-10 23:09:05 -0300

    add re.IGNORECASE because other error in de code is when the user user the upper case on email addres

--- Regular_Expressions/validade.py
+++ Regular_Expressions/validade.py
@@ -1,8 +1,8 @@
 import re
 
 email = input("What's your email? ").strip()
-
-if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$", email):
+# [a-zA-Z0-9_] or \w is the same thing
+if re.search(r"^\w+@\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
