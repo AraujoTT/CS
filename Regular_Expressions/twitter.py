61a2dc57a54afe18cff330d91f3251883a1dcea1
AraujoTT <luisthiago642@gmail.com>
2024-03-11 22:49:47 -0300

    finished topic about regular expresion and the code to find in the url the user's username

--- Regular_Expressions/twitter.py
+++ Regular_Expressions/twitter.py
@@ -2,6 +2,8 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"^(http(s)?://)?(wwww\.)?twitter\.com/", "", url)
+if matches := re.search(r"^(?:https?://)?(?:wwww\.)?twitter\.com/([a-z0-9_]+)$",url,re.IGNORECASE):
+    print(f"Username: ", matches.group(1))
 
-print(f"Username: {username}")
+# username = re.sub(r"^(http(s)?://)?(wwww\.)?twitter\.com/", "", url)
+# print(f"Username: {username}")
