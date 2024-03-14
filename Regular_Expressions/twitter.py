0bdce12a527822dca44d0c09274a68b1aff4d650
AraujoTT <luisthiago642@gmail.com>
2024-03-14 02:26:07 -0300

    reviewing about tuple, list, dict and starting oop

--- Regular_Expressions/twitter.py
+++ Regular_Expressions/twitter.py
@@ -3,7 +3,7 @@ import re
 url = input("URL: ").strip()
 
 if matches := re.search(r"^(?:https?://)?(?:wwww\.)?twitter\.com/([a-z0-9_]+)$",url,re.IGNORECASE):
-    print(f"Username: ", matches.group(1))
+    print("Username:", matches.group(1))
 
 # username = re.sub(r"^(http(s)?://)?(wwww\.)?twitter\.com/", "", url)
 # print(f"Username: {username}")
