e77c877d8d7b3ba3d23080e77fe2c69b65b4e973
AraujoTT <luisthiago642@gmail.com>
2024-03-14 03:04:46 -0300

    stating learn about how to use class in oop and initialized it

--- O_O/student.py
+++ O_O/student.py
@@ -1,10 +1,22 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
 def main():
     student = get_student()
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
-    return {"name":input("Name: "), "house":input("house: ")}
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
+    return student
 
+    # using a dictionary to return
+    # return {"name":input("Name: "), "house":input("house: ")}
+
+    # Using the list to return the value
     # name = input("Name: ")
     # house = input("house: ")
     # return [name, house]
